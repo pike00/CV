17965741a77acbca7d443afa883e29d6bf859407
Will Pike <willpike00@gmail.com>
2021-10-10 17:50:03 -0400

    Deleted gulpfile

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-
-gulp.task('minify-css', () => {
-	return gulp.src('css/*.css')
-		.pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(gulp.dest('dist'));
-});
-
